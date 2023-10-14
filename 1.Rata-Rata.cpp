10f34c70d0c7dd90534fc5bf13b18724b57107f0
Ridwanfj <147936429+Ridwanfj@users.noreply.github.com>
2023-10-14 23:05:26 +0700

    Add files via upload

---
+++ 1.Rata-Rata.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int sum=0;
+    float i;
+    int a;
+    float rata2;
+    cin>>a;
+    for (float n; i<a; ++i){
+    	cin>>n;
+    	sum=sum+n;
+	}
+	rata2=sum/i;
+	cout<<rata2;
+}
+
+
